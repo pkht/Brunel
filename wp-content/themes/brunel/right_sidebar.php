4a563d9a07d55cb1f1bf6125db3238fdece1f1ed
Phil Thomas <phil.thomas@pkht.co.uk>
2011-02-06 16:42:15 +0000

    changes for new logo and new poster

--- wp-content/themes/brunel/right_sidebar.php
+++ wp-content/themes/brunel/right_sidebar.php
@@ -2,12 +2,19 @@
     <img src="<?php bloginfo('template_directory'); ?>/images/side_image_<?=rand( 1, 9 )?>.jpg" alt="violin" />
 
     <div id="important_contact">
-	<p><span>Manager:</span><br />
-	Justin Hayward<br />07736 436675<br />
-	<a href="<?php bloginfo('url'); ?>/contact-us">Contact</a></p>
-	<p><span>Box Office:</span><br />
+    <p>
+        <span>Next Concert:</span><br /><br />
+        2nd April, 2011.<br /><br />
+        <b>Holst</b><br />
+        The Planets<br /><br />
+        <b>Butterworth</b><br />
+        The Banks of Green Willow<br <br />
+        <b>Vaughan-Williams</b><br />
+        A Pastoral Symphony<br />
+    </p>
+
+	<p><span>Box Office:</span><br /><br />
 	Providence Music<br />0117 927 6536</p>
     </div>
 
-    <div id="sponsor"><a href="http://www.burges-salmon.com" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/images/sponsor.jpg" alt="Sponsored by Burgess Salmon" /></a></div>
 </div>
