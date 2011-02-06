789f2808c6aaef7fc4fd6ca9b98255dbd4c91d18
Phil Thomas <phil.thomas@pkht.co.uk>
2011-02-06 16:50:48 +0000

    changes for new logo and new poster

--- wp-content/themes/brunel/header.php
+++ wp-content/themes/brunel/header.php
@@ -8,6 +8,8 @@
 
 <meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats -->
 
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
