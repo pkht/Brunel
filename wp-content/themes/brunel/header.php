0f3cfd75e7ed1462bb7424302a447c1bdc07cfc3
Phil Thomas <phil.thomas@pkht.co.uk>
2011-06-29 20:50:35 +0100

    changing to winter poster and stuff

--- wp-content/themes/brunel/header.php
+++ wp-content/themes/brunel/header.php
@@ -1,4 +1,5 @@
 <?php
+/*
 if( is_front_page() )
 {
     session_start();
@@ -10,6 +11,7 @@ if( is_front_page() )
         header("Location:".get_bloginfo( "url" )."/splash.php");
     }
 }
+*/
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
--- wp-content/themes/brunel/right_sidebar.php
+++ wp-content/themes/brunel/right_sidebar.php
@@ -4,14 +4,15 @@
     <div id="important_contact">
     <p>
         <span>Next Concert:</span><br /><br />
-        25th June, 2011.<br /><br />
-        <b>Rachmaninov</b><br />
-        Symphony No. 2<br /><br />
-        <b>Glinka</b><br />
-        Ruslan and Ludmilla<br <br />
-        <b>Sibelius</b><br />
-        Violin Concerto<br />
-        (Roger Huckle, violin)<br />
+        5th November, 2011.<br /><br />
+        <b>Elgar</b><br />
+        Symphony No. 3<br />
+	<i>(elaborated by Payne)</i><br /><br />
+        <b>Wagner</b><br />
+        Overture to &quot;Die Meistersinger von N&uuml;rnberg&quot;<br <br />
+        <b>Bruch</b><br />
+        Violin Concerto No. 1<br />
+        <i>(Jenny Lee, violin)</i><br />
     </p>
 
 	<p><span>Box Office:</span><br /><br />
