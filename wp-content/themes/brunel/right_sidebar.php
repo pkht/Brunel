3c41a23afaa89ef9707aa447a0e57ab0f3afd0ad
Phil Thomas <phil.thomas@pkht.co.uk>
2011-11-12 16:28:54 +0000

    new content for right site

--- wp-content/themes/brunel/right_sidebar.php
+++ wp-content/themes/brunel/right_sidebar.php
@@ -4,19 +4,19 @@
     <div id="important_contact">
     <p>
         <span>Next Concert:</span><br /><br />
-        5th November, 2011.<br /><br />
+        31st March, 2012.<br /><br />
+        <b>Saint-Saens</b><br />
+        Symphony No. 3 (Organ Symphony)<br /><br />
         <b>Elgar</b><br />
-        Symphony No. 3<br />
-	<i>(elaborated by Payne)</i><br /><br />
-        <b>Wagner</b><br />
-        Overture to &quot;Die Meistersinger von N&uuml;rnberg&quot;<br <br />
-        <b>Bruch</b><br />
-        Violin Concerto No. 1<br />
-        <i>(Jenny Lee, violin)</i><br />
+        Enigma Variations<br /><br />
+        <b>Strauss</b><br />
+        Also Sprach Zarathustra<br />
+        <i>(2001: A Space Odyssey)</i><br />
     </p>
 
-	<p><span>Box Office:</span><br /><br />
-	Providence Music<br />0117 927 6536</p>
+        <p><span>Box Office:</span><br /><br />
+        Providence Music<br />0117 927 6536</p>
     </div>
 
 </div>
+
