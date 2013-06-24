0f74e6920dbae97475e0e703f7f180694afcde45
Phil Thomas <phil.thomas@pkht.co.uk>
2013-06-24 19:02:50 +0100

    Updating right hand side bar

--- wp-content/themes/brunel/right_sidebar.php
+++ wp-content/themes/brunel/right_sidebar.php
@@ -4,18 +4,20 @@
     <div id="important_contact">
     <p>
         <span>Next Concert:</span><br /><br />
-        15th June 2013, 7.30pm.<br /><br />
-        <b>Shostakovich</b><br />
-        Symphony No. 7<br /><br />
-        <b>Rodrigo</b><br />
-        Concierto de Aranjuez<br /><br />
-        <b>Dukas</b><br />
-        The Sorcerer's Apprentice<br />
+        16th November 2013, 7.30pm.<br /><br />
+        <b>Dvo&#345;&#225;k</b><br />
+        Symphony No. 9<br />
+	"From the New World<br /><br />
+        <b>Britten</b><br />
+        Les Illuminations<br /><br />
+        <b>Stravinsky</b><br />
+        The Firebird, 1919 Suite<br />
     </p>
 
         <p><span>Box Office:</span><br /><br />
         Providence Music<br />
-        (0117) 927 6536</p>
+        (0117) 927 6536<br />
+	07951 741 179</p>
 
     </div>
 
