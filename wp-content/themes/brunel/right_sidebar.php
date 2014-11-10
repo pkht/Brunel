3587f00e9dbc3f0cf9a64128e96c046b845fb387
Nick <nick@proctors.co.uk>
2014-11-10 13:58:22 +0000

    update the sidebar with the next concert

--- wp-content/themes/brunel/right_sidebar.php
+++ wp-content/themes/brunel/right_sidebar.php
@@ -4,21 +4,17 @@
     <div id="important_contact">
     <p>
         <span>Next Concert:</span><br /><br />
-        8th November 2014, 7.30pm.<br /><br />
-        <b>Tchaikovsky</b><br />
-        Symphony No. 6<br /><br />
-        <b>Ravel</b><br />
-        Pavanne<br /><br />
-        <b>Debussy</b><br />
-        La Mer<br />
+        Saturday, 14th December 2014 6.00pm.<br /><br />
+        Bradbury Hall (Henleaze United Reform Church), Bristol<br /><br />
+        Mulled wine, soft drinks, mince pies, a Christmassy sing along, with choral and orchestral music – full of christmas cheer and merriment.<br />
     </p>
 
-    <p>
-        <span>Box Office:</span><br /><br />
-        Opus 13<br />
-        (0117) 923 0164<br />
-        <a href="mailto:shop@opus13.co.uk">shop@opus13.co.uk</a>
-	</p>
+<!--     <p> -->
+<!--         <span>Box Office:</span><br /><br /> -->
+<!--         Opus 13<br /> -->
+<!--         (0117) 923 0164<br /> -->
+<!--         <a href="mailto:shop@opus13.co.uk">shop@opus13.co.uk</a> -->
+<!-- 	</p> -->
 
     </div>
 
